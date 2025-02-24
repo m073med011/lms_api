e450939c5e17af70d72781a4d8cb9b3ff8f46ecf
m073med011 <166988534+m073med011@users.noreply.github.com>
2025-02-24 20:26:58 +0200

    fix: update MongoDB URI in .env and modify server log message

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
